d1a03705a2cdb36c6a755316de9d07d240dc4914
germainhirwa-github <higermain1@gmail.com>
2024-04-21 12:48:05 -0400

    final code

--- main.py
+++ main.py
@@ -34,10 +34,10 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3586148395&f_LF=f_AL&geoId=101356765&"
            "keywords=python&location=London%2C%20England%2C%20United%20Kingdom&refresh=true")
 
-# Click Reject Cookies Button
-time.sleep(2)
-reject_button = driver.find_element(by=By.CSS_SELECTOR, value='button[action-type="DENY"]')
-reject_button.click()
+# # Click Reject Cookies Button
+# time.sleep(2)
+# reject_button = driver.find_element(by=By.CSS_SELECTOR, value='button[action-type="DENY"]')
+# reject_button.click()
 
 # Click Sign in Button
 time.sleep(5)
@@ -52,8 +52,8 @@ password_field = driver.find_element(by=By.ID, value="password")
 password_field.send_keys(ACCOUNT_PASSWORD)
 password_field.send_keys(Keys.ENTER)
 
-# CAPTCHA - Solve Puzzle Manually
-input("Press Enter when you have solved the Captcha")
+# # CAPTCHA - Solve Puzzle Manually
+# input("Press Enter when you have solved the Captcha")
 
 # Get Listings
 print("Hello")
